8a73ab19b970fbe3ebaf50cdc3b25f015843c2e1
4centquatre <thibaudcostes11@gmail.com>
2025-12-21 16:57:35 +0100

    Add changes to app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ if st.session_state.step == "feedback":
 # Étape finale
 if st.session_state.step == "fin":
     st.write("C'est fini ! Ton score est de : "+str(st.session_state.score)+"/"+str(len(st.session_state.questions.keys())-1)+ " Bravo mon coeur t'es trop forte !")
-    for tab in st.session_state.end.items():
+    for tab in st.session_state.end.values():
         chaine = ""
         for item in tab:
             chaine = chaine + str(item) + " "
